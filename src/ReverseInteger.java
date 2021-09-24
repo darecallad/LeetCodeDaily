58c48a98e0619b86fd3e7caee307709511eb8467
darecallad <Ashane199080706@hotmail.com.tw>
2021-09-23 17:16:23 -0700

    ReverseInteger Solution1

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -22,7 +22,19 @@
 //Output: 0
 
 public class ReverseInteger {
+    // Solution1 O(log(x))
     public int reverse(int x) {
-    return x;
+        // set int rev = 0;
+        int rev = 0;
+        while( x != 0){
+            int pop = x % 10;
+            x = x / 10;
+            // if rev number > than max Integer or small than min Integer return 0;
+            if(rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if(rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            //
+            rev = rev * 10 + pop;
+        }
+        return rev;
     }
 }
