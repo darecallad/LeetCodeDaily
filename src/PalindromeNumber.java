ad43a98c221561428edf70337290d33cb6f61d73
darecallad <Ashane199080706@hotmail.com.tw>
2021-09-23 17:53:36 -0700

    Palindrome Number Solution

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -26,6 +26,14 @@
 public class PalindromeNumber {
     // Solution1
     public boolean isPalindrome(int x) {
-
+        // if x < 0 and x end with 0 but not equal to 0, return fasle;
+        if(x < 0 || (x % 10 == 0 && x != 0)) return false;
+        // otherwise x should equal to haf rev number
+        int rev = 0;
+        while( x > rev) {
+            rev = rev * 10 + x % 10;
+            x = x /10;
+        }
+        return x == rev || x ==rev / 10;
     }
 }
