5fce920b195daa3fc81cb12c2862ad8da924a6d4
JJC <johnsonchen001@outlook.com>
2021-10-04 16:41:55 -0700

    Solution3 for LCP.

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -42,4 +42,29 @@ public class LongestCommonPrefix {
         }
         return strs[0];
     }
+
+    // Solution 3 Divide and conquer
+    public String longestCommonPrefixSo3 (String[] strs){
+        // if strs == null or length == 0
+        if(strs.length == 0 || strs == null ) return "";
+            return longestCommonPrefixSo3(strs, 0 , strs.length - 1);
+    }
+    private String longestCommonPrefixSo3(String[] strs, int l , int r){
+        if(l==r) return strs[l];
+        else{
+            int mid = (l+r)/2;
+            String lcpleft = longestCommonPrefixSo3(strs, l , mid);
+            String lcpright = longestCommonPrefixSo3(strs, mid + 1 , r);
+
+            return commonPrefix(lcpleft, lcpright);
+        }
+    }
+
+    String commonPrefix(String left, String right){
+        int min = Math.min(left.length(), right.length());
+        for(int i = 0; i < min; i ++){
+            if(left.charAt(i) != right.charAt(i))   return left.substring(0,i);
+        }
+        return left.substring(0,min);
+    }
 }
