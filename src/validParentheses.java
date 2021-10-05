0256117044b517a7a73d2f42d6af723cf2387db4
JJC <johnsonchen001@outlook.com>
2021-10-05 11:09:31 -0700

    Solution for ValidParantheses

--- src/validParentheses.java
+++ src/validParentheses.java
@@ -5,9 +5,35 @@
 //Open brackets must be closed by the same type of brackets.
 //Open brackets must be closed in the correct order.
 
-public class validParentheses {
-    //Solution 1
-    public boolean isValid(String s){
+import java.util.HashMap;
+import java.util.Stack;
 
+public class validParentheses {
+    // init Hash Table
+    HashMap<Character,Character> map;
+
+    // mapping the character
+    public validParentheses(){
+        this.map = new HashMap<Character, Character>();
+        this.map.put(')','(');
+        this.map.put('}','{');
+        this.map.put(']','[');
     }
-}
+
+    public boolean isValid(String s){
+        // init Stack
+        Stack<Character> stack = new Stack<Character>();
+
+        for(int i = 0; i < s.length(); i ++){
+            char c = s.charAt(i);
+
+            // if stack empty put dummy value otherwise get the pop value
+            if(this.map.containsKey(c)){
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                if(topElement != this.map.get(c)) return false;
+        }
+            else stack.push(c);
+    }
+        return stack.isEmpty();
+}}
