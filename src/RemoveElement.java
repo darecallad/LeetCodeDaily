38f1c765e3f7396199851a7226786dd82be65286
JJC <johnsonchen001@outlook.com>
2021-10-14 16:55:31 -0700

    Remove Element Solution and Implement Str question.

--- src/RemoveElement.java
+++ src/RemoveElement.java
@@ -1,7 +1,16 @@
 
 
 public class RemoveElement {
+    // O(n)
     public int removeElement(int[] nums, int val){
-
+        // set the starting point i
+        int i = 0;
+        for( int j = 0 ; j < nums.length; j ++){
+            if(nums[j] != val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
     }
 }
