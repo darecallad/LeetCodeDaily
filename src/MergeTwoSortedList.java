8327047af1dab03c02c49a1771fcf3e0f25368ff
JJC <johnsonchen001@outlook.com>
2021-10-13 23:37:55 -0700

    MergeTwoSortedList solution1

--- src/MergeTwoSortedList.java
+++ src/MergeTwoSortedList.java
@@ -2,4 +2,30 @@
 //The list should be made by splicing together the nodes of the first two lists.
 
 public class MergeTwoSortedList {
+
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(){}
+        ListNode(int val){this.val = val;}
+        ListNode(int val, ListNode next){
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        // check if any list is null
+        if(l1 == null) return l2;
+        else if(l2 == null) return l1;
+        // compare value between l1 and l2
+        else if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else{
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    }
 }
