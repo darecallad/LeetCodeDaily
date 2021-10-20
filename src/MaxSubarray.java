e5f18ba5f115a5b84107caf21c7b4b5cf3da17d5
JJC <johnsonchen001@outlook.com>
2021-10-20 10:16:39 -0700

    Solution 2 for Max Sub Array.

--- src/MaxSubarray.java
+++ src/MaxSubarray.java
@@ -13,6 +13,16 @@ public class MaxSubarray {
     }
 
     public int maxSubArray1(int[] nums){
-        //Solution2
+        // Solution 2
+        int currentSubArray = nums[0];
+        int maxSubArray = nums[0];
+
+        // starting index 1
+        for(int i = 1; i < nums.length; i++){
+            int num = nums[i];
+            currentSubArray = Math.max(num, currentSubArray + num);
+            maxSubArray = Math.max(maxSubArray, currentSubArray);
+        }
+        return maxSubArray;
     }
 }
