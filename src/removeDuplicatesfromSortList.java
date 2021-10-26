0ac38bba49ae4b23456721915536038527265f1a
JJC <johnsonchen001@outlook.com>
2021-10-26 15:37:10 -0700

    Add run time complexity and space complexity.

--- src/removeDuplicatesfromSortList.java
+++ src/removeDuplicatesfromSortList.java
@@ -6,7 +6,7 @@ public class removeDuplicatesfromSortList {
         ListNode(int val){this.val = val;};
         ListNode(int val, ListNode next){this.val = val; this.next = next;}
     }
-
+    // run time O(n) space complexity O(1)
     public ListNode deleteDuplicates(ListNode head){
         var current = head;
 
