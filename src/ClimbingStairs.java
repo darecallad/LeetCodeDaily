848035ce150dd8a6b64ca426bbbc15a64ed8993e
JJC <johnsonchen001@outlook.com>
2021-10-26 10:45:04 -0700

    Solution 1 for Climbing Stairs.

---
+++ src/ClimbingStairs.java
@@ -0,0 +1,12 @@
+public class ClimbingStairs {
+    //Solution 1 Brute Force
+    public int climbStairs(int n){
+        return climb_Stairs(0, n);
+    }
+    public int climb_Stairs(int i, int n){
+        if(i > n) return 0;
+        if(i == n) return 1;
+
+        return climb_Stairs( i+1, n) + climb_Stairs(i+2, n);
+    }
+}
