63b5c089ba900128f13c3683632e73d1bd5dc10a
JJC <johnsonchen001@outlook.com>
2021-10-26 10:03:43 -0700

    Solution 1 for sqrt x .

--- src/sqrtX.java
+++ src/sqrtX.java
@@ -1,2 +1,12 @@
 public class sqrtX {
+    // solution1
+    public int mysqrt(int x){
+        // if int < 2 return int
+        if (x < 2) return x;
+
+        int left = (int)Math.pow(Math.E, 0.5 * Math.log(x));
+        int right = left + 1;
+
+        return (long)right * right > x ? left : right;
+    }
 }
