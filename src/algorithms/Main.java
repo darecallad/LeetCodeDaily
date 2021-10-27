dba60f187b8dc8290e28cf1dc7af1c68c8cd6a59
JJC <johnsonchen001@outlook.com>
2021-10-26 17:30:33 -0700

    Test for Array Class.

--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -1,4 +1,11 @@
 package algorithms;
 
 public class Main {
+    public static void main(String args[]){
+        Array array = new Array(3);
+        array.insert(10);
+        array.insert(20);
+        array.indexOf(20);
+        array.remove(0);
+    }
 }
