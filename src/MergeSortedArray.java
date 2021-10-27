e5074a8628ff836015b9c9faa14e21e16aae4bb6
JJC <johnsonchen001@outlook.com>
2021-10-27 10:47:35 -0700

    Solution 3 for Merge Sort Array.

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -23,4 +23,18 @@ public class MergeSortedArray {
                 nums1[p] = nums2[p2++];
         }
     }
+
+    //Solution 3
+    public void merge2(int[] nums1, int m, int[] nums2, int n){
+        int p1 = m -1;
+        int p2 = n -1;
+
+        for( int p = m + n -1 ; p >= 0 ; p--){
+            if( p2 < 0) break;
+            if( p1 >= 0 && nums1[p1] > nums2[p2])
+                nums1[p] = nums1[p1--];
+            else
+                nums1[p] = nums2[p2--];
+        }
+    }
 }
