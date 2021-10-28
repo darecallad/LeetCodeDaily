f88d96e8a49cc8d62ef60467edb396e847ad2f06
JJC <johnsonchen001@outlook.com>
2021-10-28 14:16:50 -0700

    add size and bug fixed for linklist.

--- src/algorithms/Linklist.java
+++ src/algorithms/Linklist.java
@@ -20,9 +20,13 @@ public class Linklist {
 
     private Node first;
     private Node last;
+    private int count;
 
     private boolean isEmpty(){
         return first == null;
+    }
+    private boolean oneNode(){
+        return first == last;
     }
 
     public void addLast(int item){
@@ -33,6 +37,7 @@ public class Linklist {
             last.next = node;
             last = node;
         }
+        count++;
     }
     public void addFirst( int item){
         var node = new Node(item);
@@ -42,6 +47,7 @@ public class Linklist {
             node.next = first;
             first = node;
         }
+        count++;
     }
     public int indexOf( int item){
         if(isEmpty()) return -1;
@@ -61,21 +67,31 @@ public class Linklist {
     }
     public void removeFirst(){
         if(isEmpty()) throw new NoSuchElementException();
-        if(first == last){
+        if(oneNode()){
             first = last = null;
+            count--;
             return;
         }
         var second = first.next;
         first.next = null;
         first = second;
+        count--;
     }
     public void removeLast(){
         if(isEmpty()) throw new NoSuchElementException();
+        if(oneNode()){
+            first = last = null;
+            count--;
+            return;
+        }
         var current = first;
         while(current.next != null){
             current = current.next;
         }
         current.next = null;
+        count--;
     }
-
+    public int size(){
+        return count;
+    }
 }
