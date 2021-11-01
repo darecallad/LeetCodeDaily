62c7caae7f9449651165a0636cec6e5c9bda0b79
JJC <johnsonchen001@outlook.com>
2021-11-01 16:22:16 -0700

    Reverse Stack question.

---
+++ src/algorithms/stacktest.java
@@ -0,0 +1,20 @@
+package algorithms;
+
+import java.util.Stack;
+
+public class stacktest {
+
+    public String reverse(String input){
+        if(input == null) throw new IllegalArgumentException();
+
+        Stack <Character> stack = new Stack<>();
+        for(char ch : input.toCharArray())
+            stack.push(ch);
+
+        StringBuffer stb = new StringBuffer();
+
+        while(!stack.isEmpty())
+             stb.append(stack.pop());
+        return stb.toString();
+    }
+}
