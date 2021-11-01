cce804a07ffe4190aaeb4718f5d9e45410440b87
JJC <johnsonchen001@outlook.com>
2021-11-01 15:12:25 -0700

    Binart Tree Inorder.

--- src/BinaryTreeInorder.java
+++ src/BinaryTreeInorder.java
@@ -1,2 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+
 public class BinaryTreeInorder {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // Solution 1
+    public List<Integer> inorderTraversal(TreeNode root){
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List <Integer> res){
+        if(root != null){
+            if( root.left != null){
+                helper(root.left, res);
+            }
+            res.add(root.val);
+            if(root.right != null){
+                helper(root.right, res);
+            }
+        }
+    }
+    //Solution 2
+    public List <Integer> inorderTraversal1(TreeNode root){
+        List <Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode current = root;
+        while(current != null || !stack.isEmpty()){
+            while(current != null){
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            res.add(current.val);
+            current = current.right;
+        }
+        return res;
+    }
 }
