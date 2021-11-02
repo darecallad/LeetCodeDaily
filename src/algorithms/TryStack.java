6be339a0733cb4a30efbfd9bee3f762b0e232085
JJC <johnsonchen001@outlook.com>
2021-11-02 10:50:53 -0700

    Stack updated.

--- src/algorithms/TryStack.java
+++ src/algorithms/TryStack.java
@@ -4,29 +4,24 @@ import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class TryStack {
+
+    private int count  = 0;
     private int[] arr = new int[5];
-    private int count = 0;
-    public TryStack(){}
+
+    public void push(int item){
+        if(count == arr.length) throw new IllegalStateException();
+        arr[count++] = item;
+    }
     public boolean isEmpty(){
         return count == 0;
     }
-    public void push(int item){
-        if(count == arr.length ){
-            int[] newarr = new int[arr.length * 2];
-            for(int i = 0; i < count ; i ++){
-                newarr[i] = arr[i];
-            }
-            arr = newarr;
-        }
-        arr[count++] = item;
-    }
     public int pop(){
-        if(isEmpty()) throw new NoSuchElementException();
+        if(isEmpty()) throw new IllegalStateException();
         return arr[--count];
     }
     public int peek(){
-        if(isEmpty()) throw new NoSuchElementException();
-        return arr[count -1];
+        if(isEmpty()) throw new IllegalStateException();
+        return arr[count - 1];
     }
     @Override
     public String toString(){
